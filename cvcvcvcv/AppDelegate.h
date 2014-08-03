1a0af4ac7849cd62dae51bb08f0a58c0afb1dd8b
AnilSB <general@Anils-MacBook-Pro-2.local>
2014-08-03 08:24:38 +0530

    ffgfg
    bnbnbnbnbn

---
+++ cvcvcvcv/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cvcvcvcv
+//
+//  Created by General on 03/08/14.
+//  Copyright (c) 2014 com.tttt.hhh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;;
+
+@end
